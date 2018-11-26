4c475eff724860ca09fd058939f4f4d166207ca6
gelosofederico <gelosofederico@gmail.com>
2018-11-26 19:16:07 -0300

    Ej2 fix copy

--- Ej2.py
+++ Ej2.py
@@ -3,11 +3,6 @@ import argparse
 import math
 
 def main(file_stations,file_railways,people_per_wagon,min_wagon_per_line,num_wagons_in_system):
-    # grapg = create_subway_graph(file_stations,file_railways,people_per_wagon,min_wagon_per_line,num_wagons_in_system)
-    # test_graph = [ ["a",[[2,5],[4,20]]] , ["b",[ [5,1],[3,300],[6,100] ]] , ["c",[[0,5],[5,10]]],["d",[[1,300],[4,10]]],["e",[[0,20],[3,10]]],["f",[[1,1],[2,10],[6,100]]],["g",[[5,100],[1,100]]]]
-    # print(test_graph)
-    # max_f = ford_fulkerson(test_graph,0,1)
-    # print(max_f)
     subway_people_graph,station_dict = create_subway_graph(file_stations,file_railways,people_per_wagon)
     necessary_flow_source = 0
     necessary_flow_sink = 0
@@ -18,20 +13,18 @@ def main(file_stations,file_railways,people_per_wagon,min_wagon_per_line,num_wag
     if necessary_flow_source != necessary_flow_sink:
         print("La fuente y el sumidero tienen cantidades distintas. La gente no se crea ni se destruye, solo se transforma (?")
         exit()
-    for i in subway_people_graph:
-        print(i)
     max_f_people,subway_people_graph_residual = ford_fulkerson(subway_people_graph,0,1)
-    for i in subway_people_graph_residual:
-        print(i)
-    print(max_f_people)
     if max_f_people != necessary_flow_source:
         print("No es factible debido al estado de las vias")
     lines_railways_graph = create_lines_railways_graph(subway_people_graph,station_dict,subway_people_graph_residual,people_per_wagon,min_wagon_per_line)
-    for i in lines_railways_graph:
-        print(i)
     max_f_trains,res_graph = ford_fulkerson(lines_railways_graph,0,1)
-    print(max_f_trains)
-
+    necessary_num_wagons = 0
+    for i in lines_railways_graph[1][1]:
+        necessary_num_wagons += i[1]
+    if necessary_num_wagons > num_wagons_in_system:
+        print("No es factible debido a la cantidad de vagones")
+        exit()
+    print("Es factible segun este modelo. Se necesitarian "+str(necessary_num_wagons) + " vagones.")
 
     return 
 
@@ -40,6 +33,9 @@ def create_lines_railways_graph(subway_people_graph,station_dict,subway_people_g
     # Esto consiste en ver cuantos vagones se necesitan para cada tramo, que es
     # la cantidad de gente en el tramo sobre la capacidad de un vagon redondeado
     # para arriba
+    # Para encontrar los tramos se hace un BFS, y no se pasa entre líneas
+    # (suponemos que las combinaciones admiten cantidad de gente infinita,
+    # ya que no necesitan vagones)
     # A su vez se debe separar en líneas para cumplir la restricción del mínimo
     # por línea. Para esto se verá si cumplen la cantidad con los que necesitan
     # y si no, se agregará una capacidad al sumidero que es la demanda de la
@@ -49,22 +45,24 @@ def create_lines_railways_graph(subway_people_graph,station_dict,subway_people_g
     visited = [False] * (len(subway_people_graph))
     visited[0] = True
     visited[1] = True # Son fuente y sumidero
+    num_visited = 2
+
     from collections import deque
     queue = deque([2])
+    # Creo el grafo
     railways_graph = []
     railways_graph.append(["source",[]])
     railways_graph.append(["sink",[]])
+    # Creo la primer línea. lines_nodes_pos tiene las posiciones de las líneas
     this_line = 2
     railways_graph.append(["Line"+str(this_line),[[0,float("inf")]]])
     railways_graph[0][1].append([this_line,float("inf")])
     lines_nodes_pos = [2]
-    num_visited = 2
 
     while num_visited!=len(subway_people_graph):
-        print(num_visited)
         num_visited+=1
         if not queue:
-            print(visited)
+            # Voy a empezar una nueva línea
             next_pos = visited.index(False)
             queue.append(next_pos)
             visited[next_pos] = True
@@ -72,7 +70,6 @@ def create_lines_railways_graph(subway_people_graph,station_dict,subway_people_g
             # Revisamos si cumple el mínimo de línea
             wagons_needed = 0
             for i in railways_graph[this_line][1]:
-                print(i)
                 if i[1] != float("inf"):
                     wagons_needed += i[1]
             if wagons_needed < min_wagon_per_line:
@@ -158,6 +155,9 @@ def create_subway_graph(file_stations,file_railways,max_people_per_wagon):
     # El grafo se utiliza con lista de adyacencia:
     # Es una lista de listas. Cada lista aquí contiene, primero, el nombre de
     # la estación y luego una lista con de aristas, las cuales tienen 
+    # entonces grafo[0] es una lista con: nombre, lista de aristas incidentes
+    # grafo[0][1] es la lista de aristas que inciden en el nodo
+    # Una arista es una lista con la posición del nodo al que incide y la capacidad
     stations_info = read_stations(file_stations)
     railways_info = read_railways(file_railways)
     # subway_people_graph es con todas las cantidades consideradas gente
@@ -192,7 +192,6 @@ def create_subway_graph(file_stations,file_railways,max_people_per_wagon):
 
 
 def ford_fulkerson(graph,pos_source,pos_sink):
-    # Asumimos que el primer nodo es la fuente y el segundo el sumidero
     import copy
     graph_residual = copy.deepcopy(graph)
     source = graph_residual[pos_source]
@@ -235,6 +234,7 @@ def bfs_path(graph,start,finish):
     # En la cola van el nodo en el que está, el camino hasta ese y el cuello de
     # botella
     from collections import deque
+    import copy
     queue = deque([[start,[],-1]])
     visited = [False]*len(graph)
     visited[start] = True
@@ -253,7 +253,7 @@ def bfs_path(graph,start,finish):
                 edge_value = graph[current_node_pos][1][i][1]
                 new_bottleneck = min(current_bottleneck,edge_value)
                 if visited[next_node_pos]==False and edge_value>0:
-                    new_path = current_path.copy()
+                    new_path = copy.copy(current_path)
                     new_path.append(current_node_pos)
                     visited[next_node_pos] = True
                     if current_bottleneck == -1:
